d95cb464a8d353ef66f7306446a9dfde4c747226
Lucky <sharma0306lucky@gmail.com>
2026-04-11 23:29:51 +0530

    Initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+const User = require ("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.registerUser = async (req , res) => {
+    try{
+        const{name , email , password } =req.body;
+        const userExists = await User.findOne({email});
+    if(userExists){
+        return res.status(400).json({message:"User already exists"});
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password , salt);
+}catch(error) {
+    res.status(500).json({ error: error.message });
+  }
+}
