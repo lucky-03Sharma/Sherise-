abb1284d65afc80eba3d66ea7658df911348a875
Lucky <sharma0306lucky@gmail.com>
2026-04-12 21:05:54 +0530

    adding middleware

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { createComplaint } = require("../controllers/complaintController");
+const protect = require("../middlewares/authMiddleware");
+const { create } = require("../models/User");
 
-router.post("/create", createComplaint);
+router.post("create", protect , createComplaint);
 module.exports = router;
