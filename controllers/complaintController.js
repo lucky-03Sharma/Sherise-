93118598c310b1bad61dee67ddb78ebe2021b130
Lucky <sharma0306lucky@gmail.com>
2026-04-13 20:43:20 +0530

    update request

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -1,6 +1,6 @@
 const Complaint = require("../models/Complaint");
 
-const createComplaint = async (req, res) => {
+exports.createComplaint = async (req, res) => {
     try {
         const { name, type, description, location, isAnonymous } = req.body;
 
@@ -20,5 +20,34 @@ const createComplaint = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+exports.getComplaintsById = async (req, res) => {
+    try {
+        const complaints = await Complaint.findById(req.params.id);
+        if (!complaints) {
+            return res.status(404).json({ message: "Complaint not found" });
+        }
+        res.json(complaints);
+        } catch (error) {
+            res.status(500).json({ error: error.message });  
+    }
+};
 
-module.exports = { createComplaint };
+exports.updateComplaint = async (req, res) => {
+    try {
+        const complaint = await Complaint.findById(req.params.id);
+        if (!complaint) {
+            return res.status(404).json({ message: "Complaint not found" });
+        }
+        if (complaint.userId.toString() !== req.user.id) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const updated = await Complaint.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.json(updated);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createComplaint } = require("../controllers/complaintController");
+const { createComplaint , getComplaintsById , updateComplaint } = require("../controllers/complaintController");
 const protect = require("../middlewares/authMiddleware");
 const { create } = require("../models/User");
-
+router.get("/:id", getComplaintsById);
+router.put("/:id", updateComplaint);
 router.post("/create", protect , createComplaint);
 module.exports = router;
