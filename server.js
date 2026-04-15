d4a7bfb02ce8105a0b57920954a7f8ea19e8b400
Lucky <sharma0306lucky@gmail.com>
2026-04-15 21:49:10 +0530

    therapy routes

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const jwtwebtoken = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const therapyRoutes = require("./routes/therapyRoutes");
 const consultationRoutes = require("./routes/ConsultationRoutes");
 const complaintRoutes = require("./routes/complaintRoutes");
 const authRoutes = require("./routes/authRoutes");
-dotenv.config();
 const app = express();
+dotenv.config();
+
 app.use(express.json());
 app.use("/api/complaints", complaintRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/therapy", therapyRoutes);
 app.use("/api/consultations" , consultationRoutes);
 app.get("/" , (req , res)=>{
     res.send("SheRise API is running");
 })
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
