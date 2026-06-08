5d0c4a763a46fb558ee877697e674af66321f1e4
Lucky <sharma0306lucky@gmail.com>
2026-06-08 22:56:45 +0530

    Added consultation CRUD APIs

--- backend/routes/ConsultationRoutes.js
+++ backend/routes/ConsultationRoutes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middlewares/authMiddleware");
 
-const { createConsultation, getMyConsultations } = require("../controllers/consultationController");
+const { createConsultation, 
+    getMyConsultations, 
+    deleteConsultation } = require("../controllers/consultationController");
 
 router.post("/create", protect, createConsultation);
 router.get("/my", protect, getMyConsultations);
+router.delete("/delete/:id", protect, deleteConsultation);
 module.exports = router;
